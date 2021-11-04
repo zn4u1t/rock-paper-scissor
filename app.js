5f99263ad080c5c4ac080e08332ded2ceb304fde
zn4u1t <33137041+zn4u1t@users.noreply.github.com>
2021-11-04 11:49:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
@@ -7,3 +7,61 @@ const result_div = document.querySelector('.result');
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
+
+
+function getComputerChoice() {
+	const choices = ['r', 'p', 's'];
+	const randomNumber = Math.floor(Math.random() * 3);
+	return choices[randomNumber];
+}
+
+function win(user, computer) {
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_div.innerHTML = userChoice + 'beats' + computerChoice + '. You Win!!!';
+}
+
+function lose() {
+	computerScore++;
+}
+
+function draw() {
+	console.log('DRAW');
+}
+
+
+function game(userChoice) {
+	const computerChoice = getComputerChoice();
+	switch (userChoice + computerChoice) {
+		case 'rs':
+		case 'pr':
+		case 'sp':
+			win(userChoice, computerChoice);
+			break;
+		case 'rp':
+		case 'ps':
+		case 'sr':
+			lose(userChoice, computerChoice);
+			break;
+		case 'rr':
+		case 'pp':
+		case 'ss':
+			draw(userChoice, computerChoice);
+			break;
+	}
+}
+
+function main() {
+	rock_div.addEventListener('click', function() {
+		game('r');
+	})
+	paper_div.addEventListener('click', function() {
+		game('p');
+	})
+	scissors_div.addEventListener('click', function() {
+		game('s');
+	})
+}
+
+main();
